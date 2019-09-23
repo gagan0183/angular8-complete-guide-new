fe83e1d8678c984475ad3b28af9539132d7be3b6
gagan0183 <g.deepsingh1@gmail.com>
2019-09-23 21:01:24 +0530

    created two new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { FormsModule } from '@angular/forms';
-import { AssignmentsComponent } from './assignments/assignments.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    AssignmentsComponent
+    CockpitComponent,
+    ServerElementComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
