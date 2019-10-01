4b06d2077011aed7470e9bf6bc0b43cfc28a9177
gagan0183 <g.deepsingh1@gmail.com>
2019-10-01 18:15:12 +0530

    custom properties binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type: 'server', name: 'TestServer', content: 'This is server'}];
 }
