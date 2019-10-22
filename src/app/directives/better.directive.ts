a2f408c29f769e1704516f0fc9386c51bc4d5fe0
gagan0183 <g.deepsingh1@gmail.com>
2019-10-22 21:20:10 +0530

    host listener to listen to host events

--- src/app/directives/better.directive.ts
+++ src/app/directives/better.directive.ts
@@ -1,4 +1,4 @@
-import { OnInit, Renderer2, Directive, ElementRef } from '@angular/core';
+import { OnInit, HostListener, Renderer2, Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[better]'
@@ -7,6 +7,14 @@ export class BetterDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
+    /this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', '#eeeeee');
+  }
+
+  @HostListener('mouseenter') mouseenter(event: Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', '#eeeeee');
+  }
+
+  @HostListener('mouseleave') mouseleave(event: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', '#e1e1e1');
   }
 }
